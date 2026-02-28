07cc134b73db9e6128013867ed0697f9ee366fd1
Nikola Stefanovic <50869369+IronnKnight@users.noreply.github.com>
2026-02-28 16:36:12 +0100

    02: Redux store and core app wiring (#3)

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,1 +1,7 @@
-export {};
+import type { PropsWithChildren } from "react";
+import { Provider } from "react-redux";
+import { store } from "./store";
+
+export const AppProviders = ({ children }: PropsWithChildren) => {
+  return <Provider store={store}>{children}</Provider>;
+};
