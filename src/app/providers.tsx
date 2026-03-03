d9c37ac2a3512b93de32e01a07b76801a2a6cda9
Nikola Stefanovic <nikola.sensei94@gmail.com>
2026-03-03 09:27:20 +0100

    10: Remove dispatch access via store in provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,23 @@
 import { useEffect, type PropsWithChildren } from "react";
 import { Provider } from "react-redux";
 import { store } from "@/app/store";
+import { useAppDispatch } from "@/app/hooks";
 import { loadCurrentUser } from "@/features/auth/authSlice";
 
-export const AppProviders = ({ children }: PropsWithChildren) => {
-  useEffect(() => {
-    store.dispatch(loadCurrentUser());
-  }, []);
+const StartupLoader = ({ children }: PropsWithChildren) => {
+  const dispatch = useAppDispatch();
 
-  return <Provider store={store}>{children}</Provider>;
+  useEffect(() => {
+    dispatch(loadCurrentUser());
+  }, [dispatch]);
+
+  return <>{children}</>;
+};
+
+export const AppProviders = ({ children }: PropsWithChildren) => {
+  return (
+    <Provider store={store}>
+      <StartupLoader>{children}</StartupLoader>
+    </Provider>
+  );
 };
